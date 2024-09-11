e903150a8662975f0e6b28f6216cc1c415de1a5b
rida asad <riasa0895@oslomet.no>
2024-09-12 00:44:32 +0200

    finne størst tall

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,33 +1,16 @@
-import java.util.Arrays;
+import java.util.NoSuchElementException;
 
-public class Oblig1 {
-    public static int maks(int[] a) {
-        if (a.length < 1)
-            throw new java.util.NoSuchElementException("Tabellen er tom!");
-
-        if (a.length <= 2) {
-            return a[0];
-        }
-
-        if (a[1] > a[2]) {
-            int temp = a[1];
-            a[1] = a[2];
-            a[2] = temp;
-        }
-
-        return a[1];
+public static int maks(int[] a) {
+    if (a.length == 0) {
+        throw new NoSuchElementException("Tabellen er tom!");
     }
-
-    public static void swapIfGreater(int[] a) {
-        if (a.length < 2) {
-            System.out.println("Tabellen har færre enn to elementer.");
-            return;
-        }
-
-        if (a[0] > a[1]) {
-            int temp = a[0];
-            a[0] = a[1];
-            a[1] = temp;
+    for (int i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            int temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
         }
     }
+    return a[a.length - 1];
 }
+
