ea2ee0cbe73f0929e8a4f888fd4323ce9d27c4bc
rida asad <riasa0895@oslomet.no>
2024-09-12 01:04:44 +0200

    svare på oppg1

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -14,3 +14,42 @@ public static int maks(int[] a) {
     return a[a.length - 1];
 }
 
+/*
+(a) Hvor mange sammenlikninger blir det, som en funksjon av n?
+antall sammenligninger av funksjonen n utifra arrayet er n-1.
+
+b) Når blir det færrest ombyttinger?
+ser ikke ut som det er no færrest ombyttinger siden byttet skjer når arrayet er allerede sortert i stigende rekke.
+
+(c) Når blir det flest ombyttinger?
+Flest bytter foregår når arrayet er sortert i synkende rekkefølge. Her skal hvert par av elementer byttes,
+så antallet bytte er n - 1.
+
+(d) Hvor mange ombyttinger blir det i gjennomsnitt?
+Gjennomsnittlig er antall ombyttinger n/2.
+*/
+
+public static int ombyttinger(int[]a) {
+    int ombytte = 0;
+    for (int i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            int temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+            ombytte++;               //nåværende element større enn neste, utføres bytte
+        }
+    }
+    return ombytte;
+
+}
+
+/* Denne maks-metoden er dårligere enn den som ble brukt tidligere siden den er mindre effektiv og tar mer
+ tid å kunne utføre pga strl på arrayet.
+*/
+
+
+
+
+
+
+
