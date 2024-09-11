ab516e99a404df4048f1569e9d3a7ef9aef5ff99
rida asad <riasa0895@oslomet.no>
2024-09-12 01:48:21 +0200

    rename

--- src/Sortert.java
+++ src/Sortert.java
@@ -1,15 +1,15 @@
-public class ulikeVerdier {
+public class Sortert {
     public static int antallUlikeSortert(int[] a) {
-        if (a.length == 0)
+        if (a.length == 0) {
             return 0;
-        {
         }
+//ser vis tabellen er tom
         if (!sortert(a)) {
             throw new IllegalStateException("Tabellen er ikke sortert stigende i stigende rekkefølge!");
         }
         int ulikeVerdier = 1;
         for (int i = 1; i < a.length - 1; i++) {
-            if (a[i] != a[i - 1]) {
+            if (a[i] != a[i - 1]) { //ser antall ulike elementer i arrayet
                 ulikeVerdier++;
             }
         }
