bb437e3cd80dbbf9835ed8f5b1f5d27bee336acd
rida asad <riasa0895@oslomet.no>
2024-09-12 17:41:58 +0200

    ny oppg

--- src/uSortert.java
+++ src/uSortert.java
@@ -1,2 +1,27 @@
 public class uSortert {
+
+    public static int antallUlikeUsortert(int[] a) {
+
+        if (a.length == 0)
+            return 0;     //sjekker om tabellen er tom
+
+        int antallUlike = 1;
+        boolean sistUnik = true;
+
+        for (int i = 1; i < a.length; i++) { //går gjennom arrayet fra 2. element
+            if (a[i] > a[i - 1]) {
+                if (sistUnik) {
+                    antallUlike++;
+                    sistUnik = false;
+                }
+            } else if (a[i] == a[i - 1]) {
+                sistUnik = true;
+            } else {
+                antallUlike++;
+                sistUnik = false;
+            }
+        }
+
+        return antallUlike;
+    }
 }
